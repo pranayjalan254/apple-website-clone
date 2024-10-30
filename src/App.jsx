200ae522b6a25e48da24d7bdb02281a8dcd20362
Pranay <pranayjalan.work@gmail.com>
2024-10-30 10:03:59 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h1 >React App</h1>
-    </div>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   );
 };
 export default App;
